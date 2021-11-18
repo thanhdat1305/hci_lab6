49f2a9282b3ade45b56fdde20b1af111fc9476e8
Nguyễn Thành Đạt <37068155+thanhdat1305@users.noreply.github.com>
2021-11-18 10:05:30 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,81 +21,43 @@ $(function() {
 });
 
 $(document).ready(function(){
-
     var checkU = false;
-
     var index = -1;
-
-$('#tendangnhap').blur(function(){
-
+    var listTenDangNhap = new Array("itstudent", "itforstudent", "gtnm");
+    var listMatKhau = new Array( "123","12345","1234");
+ $('#tendangnhap').blur(function(){
     var tendangnhap = $('#tendangnhap').val();
 
-    var listTenDangNhap = new Array("itstudent","itforstudent","gtnm");
-
-    //alert(listTenDangNhap[0]);
-
-    //var listMatKhau = new Array("123","12345","1234");
-
-    for(var i = 0; i < listTenDangNhap.length; i++) {
-
-         if(tendangnhap==listTenDangNhap[i]){
-
-             checkU = true;
-
-             index = i;
-
-             break;
-
-         }else{
-
-             checkU=false;
-
-         }
-
-}
-
-if(checkU==true){
-
-    alert("Username hợp lệ");
-
-}else{
-
-    alert("Username không tồn tại");
-
-}
-
-if(index!=-1){
-
-$('#matkhau').blur(function(){
-
-    var checkM = false;
-
-    var matkhau = $('#matkhau').val();
-
-    //var listTenDangNhap = new Array("itstudent","itforstudent","gtnm");
-
-    //alert(listTenDangNhap[0]);
-
-    var listMatKhau = new Array("123","12345","1234");
-
-    if(matkhau==listMatKhau[index]){
-
-         alert("Đăng nhập thành công");
-
-    }else{
-
-         alert("Sai mật khẩu");
-
+    //alert(listTenDangNhap[0]):
+    //var listMatkhau = new Array("123"."12345","1234");
+for(var i = 0; i < listTenDangNhap.length; i++) {
+      if(tendangnhap==listTenDangNhap[i]){
+        checkU = true;
+        index = i;
+        break;
+      }else{
+        checkU=false;
+      }
     }
 
-    });
+ if(checkU==true){
+   alert("Username hợp lệ");
+ }else {
+    alert("usemame không tồn tại");
+ }
+});
 
-}else{
-
-    alert("Vui lòng kiểm tra lại tên đăng nhập");
-
-}
-
+ $('#matkhau').blur(function(){
+    var checkM = false;
+    var matkhau = $('#matkhau').val();
+    //var listTenDangNhap = new Array("itstudent"."itforstudent","gtnm"):
+    //alert(listTenDangNhap[0]):
+if (matkhau==listMatKhau[index]){
+      alert("Đăng nhập thành công");
+ } else{
+      alert("Sai mật khẩu");
+    }
 });
 
 });
+
